6f38eb98740154b003220910c0124de13cd5238c
Nadeshan <21106471+Nadeshan@users.noreply.github.com>
2019-06-06 18:11:27 +0530

    Introducing unit test

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,11 +2,20 @@ package clinic.programming.training;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.String;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Application {
 	
+	public int countWords(String words){
+			
+			String[] seperateWords = StringUtils.split(words, " ");
+			return (seperateWords == null) ?0 : seperateWords.length;
+		}
+	
 	public void greet(String[] arg){
 		
+			
 		List<String> greetings = new ArrayList<>();
 		greetings.add("Hello");
 		greetings.add("Vanakkam");
@@ -34,5 +43,7 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet(args);//Need to provide commandline arguements for greeting..!
+		int count = app.countWords("I have  four or five words");
+		System.out.println("Word Count : "+count);
     }
 }
